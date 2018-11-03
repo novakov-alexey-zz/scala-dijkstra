14296fff25019996bd8fa1b4c8334433a4fd5859
Alexey Novakov <novakov.alex@gmail.com>
2018-11-03 15:08:45 +0100

    add scaladocs

--- src/dijkstra/dijkstraSpAlg.scala
+++ src/dijkstra/dijkstraSpAlg.scala
@@ -3,6 +3,10 @@ package dijkstra
 import scala.annotation.tailrec
 import scala.collection.mutable
 
+/**
+  * Algorithm is based on Java version implemented by Princeton University https://algs4.cs.princeton.edu/44sp/
+  */
+
 final case class DirectedEdge(from: Int, to: Int, weight: Double)
 
 final case class EdgeWeightedDigraph(adj: mutable.LinkedHashMap[Int, List[DirectedEdge]] = mutable.LinkedHashMap())
@@ -10,6 +14,13 @@ final case class EdgeWeightedDigraph(adj: mutable.LinkedHashMap[Int, List[Direct
 object EdgeWeightedDigraphOps {
 
   implicit class EdgeWeightedDigraphOps(g: EdgeWeightedDigraph) {
+    /**
+      * Adds directed edge 'e' to EdgeWeightedDigraph by creating a full-copy adjacency list, i.e. this operation
+      * leaves current graph 'g' immutable
+      *
+      * @param e - DirectedEdge to add into the graph 'g'
+      * @return returns new graph with edge 'e' added to it
+      */
     def addEdge(e: DirectedEdge): EdgeWeightedDigraph = {
       val list = g.adj.getOrElse(e.from, List.empty)
       val adj = g.adj.clone() += (e.from -> (list :+ e))
@@ -20,7 +31,13 @@ object EdgeWeightedDigraphOps {
 }
 
 object ShortestPath {
-
+  /**
+    * Function tries to find a shortest path from source vertex to all other vertices in the graph
+    *
+    * @param g       EdgeWeightedDigraph to find a shortest path
+    * @param sourceV source vertex to find a shortest path from
+    * @return return either error as string when input parameters are invalid or return shortest path result
+    */
   def run(g: EdgeWeightedDigraph, sourceV: Int): Either[String, ShortestPathCalc] = {
     val size = g.adj.size
 
@@ -32,7 +49,8 @@ object ShortestPath {
       //init source distance and add to the queue
       distTo(sourceV) = 0.0
       val sourceDist = (sourceV, distTo(sourceV))
-      val queue = mutable.PriorityQueue[(Int, Double)](sourceDist)((a, b) => a._2.compareTo(b._2))
+      val sortByWeight: Ordering[(Int, Double)] = (a, b) => a._2.compareTo(b._2)
+      val queue = mutable.PriorityQueue[(Int, Double)](sourceDist)(sortByWeight)
 
       while (queue.nonEmpty) {
         val (minDestV, _) = queue.dequeue()
@@ -52,7 +70,18 @@ object ShortestPath {
   }
 }
 
+/**
+  *
+  * @param edgeTo a sequence of Optional DirectedEdge. Some value is set when source vertex 'sourceV' has
+  *               an edge to specific i vertex
+  * @param distTo a sequence of distances from source vertex to a specific i vertex
+  */
 class ShortestPathCalc(edgeTo: Seq[Option[DirectedEdge]], distTo: Seq[Double]) {
+  /**
+    *
+    * @param v vertex to get the path for
+    * @return returns error when v is invalid or sequence of edges which form the path from source vertex to v vertex
+    */
   def pathTo(v: Int): Either[String, Seq[DirectedEdge]] = {
 
     @tailrec
@@ -65,9 +94,19 @@ class ShortestPathCalc(edgeTo: Seq[Option[DirectedEdge]], distTo: Seq[Double]) {
     hasPath(v).map(b => if (!b) Seq() else go(List(), v))
   }
 
+  /**
+    *
+    * @param v vertex to check whether path from source vertex to v vertex exists
+    * @return returns error when v is invalid or Boolean when some path from source vertex to vertex v exists
+    */
   def hasPath(v: Int): Either[String, Boolean] =
     distTo.lift(v).map(_ < Double.PositiveInfinity).toRight(s"Vertex $v does not exist")
 
+  /**
+    *
+    * @param v vertex to get the distance for
+    * @return returns error when v is invalid or the Double distance which is a sum of weights
+    */
   def distToV(v: Int): Either[String, Double] =
     distTo.lift(v).toRight(s"Vertex $v does not exist")
 }
