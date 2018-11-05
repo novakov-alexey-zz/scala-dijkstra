a6812c2515e375436422120d8b23345060e822ff
Alexey Novakov <novakov.alex@gmail.com>
2018-11-05 11:45:24 +0100

    improve unit test

--- src/dijkstra/ShortestPathTest.scala
+++ src/dijkstra/ShortestPathTest.scala
@@ -1,6 +1,9 @@
 package dijkstra
 
+import scala.collection.mutable.ArrayBuffer
+
 object ShortestPathTest extends App {
+
   import dijkstra.EdgeWeightedDigraphOps._
 
   val g = EdgeWeightedDigraph()
@@ -26,12 +29,20 @@ object ShortestPathTest extends App {
   val path = sp.right.get.pathTo(6)
   assert(path.isRight)
 
-  val actual = path.right.get
-  assert(actual.nonEmpty)
+  val actualPath = path.right.get
+  assert(actualPath.nonEmpty)
 
-  val expected = Seq(
+  val expectedPath = Seq(
     DirectedEdge(0, 2, 0.26), DirectedEdge(2, 7, 0.34), DirectedEdge(7, 3, 0.39), DirectedEdge(3, 6, 0.52)
   )
 
-  assert(actual == expected)
+  assert(actualPath == expectedPath, "list of edges does not match")
+
+  val expectedDist = ArrayBuffer(0, 1.05d, 0.26d, 0.99d, 0.38d, 0.73d, 1.51d, 0.60d)
+  val actualDist = (0 to expectedDist.length).flatMap(i => sp.right.get.distToV(i).map(roundDouble).toOption)
+
+  assert(actualDist == expectedDist, "distances from source vertex to all other vertices do not match")
+
+  def roundDouble(d: Double): Double =
+    BigDecimal(d).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
 }
